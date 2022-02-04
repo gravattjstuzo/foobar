fd7dd38880e5b5eee5f783996ad2f3e93f9969fb
gravattjstuzo <gravattjstuzo@users.noreply.github.com>
2022-02-04 19:47:17 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="gravattjstuzo"
+original_name="foobar"
+original_urlname="foobar"
+original_description="Awesome foobar created by gravattjstuzo"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv foobar $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- foobar/base.py
+++ foobar/base.py
@@ -1,7 +1,7 @@
 """
-project_name base module.
+foobar base module.
 
-This is the principal module of the project_name project.
+This is the principal module of the foobar project.
 here you put your main classes and objects.
 
 Be creative! do whatever you want!
